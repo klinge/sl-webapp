4074f9dbf31f3bedb04753d286b33007b4507c8b
Johan Klinge <johan.klinge@gmail.com>
2024-05-20 16:33:05 +0200

    Implemented getMembersByRoleName

--- models/MedlemRepository.php
+++ models/MedlemRepository.php
@@ -23,12 +23,20 @@ class MedlemRepository
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-
-    public function getMembersByRollName($rollId)
+    // Query Medlem, Roll, and Medlem_Roll tables
+    // to find members with a specified roll_namn
+    public function getMembersByRollName($rollName)
     {
-        // Implement logic to query Medlem, Roll, and Medlem_Roll tables
-        // to find members with the specified roll ID
-        // ...
+        $query = "SELECT m.id,m.fornamn, m.efternamn, r.roll_namn
+            FROM  Medlem m
+            INNER JOIN Medlem_Roll mr ON mr.medlem_id = m.id
+            INNER JOIN Roll r ON r.id = mr.roll_id
+            WHERE r.roll_namn = :rollnamn
+            ORDER BY m.efternamn ASC;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':rollnamn', $rollName);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     private function getDatabaseConn()
