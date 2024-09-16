aa3343da8b29eace33b15dbcd9424813bb6a51c4
Johan Klinge <johan.klinge@gmail.com>
2024-09-16 18:27:42 +0200

    Moved routes to RouteConfig

--- App/Application.php
+++ App/Application.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Dotenv\Dotenv;
 use AltoRouter; //https://dannyvankooten.github.io/AltoRouter/
+use App\Config\RouteConfig;
 use Exception;
 use App\Middleware\MiddlewareInterface;
 use App\Middleware\AuthorizationMiddleware;
@@ -28,57 +29,16 @@ class Application
         $this->addMiddleware(new AuthorizationMiddleware($this));
     }
 
-    private function setupRouter()
+    private function setupRouter(): void
     {
         $this->router = new AltoRouter();
         $this->router->setBasePath('/sl-webapp');
 
-        // Setup your routes here
-        $this->router->map('GET', '/', 'HomeController#index', 'home');
-
-        $this->router->map('GET', '/medlem', 'MedlemController#list', 'medlem-list');
-        $this->router->map('GET', '/medlem/json', 'MedlemController#listJson', 'medlem-list-json');
-        $this->router->map('GET', '/medlem/[i:id]', 'MedlemController#edit', 'medlem-edit');
-        $this->router->map('POST', '/medlem/[i:id]', 'MedlemController#save', 'medlem-save');
-        $this->router->map('GET', '/medlem/new', 'MedlemController#new', 'medlem-new');
-        $this->router->map('POST', '/medlem/new', 'MedlemController#insertNew', 'medlem-create');
-        $this->router->map('POST', '/medlem/delete', 'MedlemController#delete', 'medlem-delete');
-
-        $this->router->map('GET', '/betalning', 'BetalningController#list', 'betalning-list');
-        $this->router->map('GET', '/betalning/[i:id]', 'BetalningController#getBetalning', 'betalning-edit');
-        $this->router->map('GET', '/betalning/medlem/[i:id]', 'BetalningController#getMedlemBetalning', 'betalning-medlem');
-        $this->router->map('POST', '/betalning/create', 'BetalningController#createBetalning', 'betalning-create');
-        $this->router->map('POST', '/betalning/delete/[i:id]', 'BetalningController#deleteBetalning', 'betalning-delete');
-
-        $this->router->map('GET', '/segling', 'SeglingController#list', 'segling-list');
-        $this->router->map('GET', '/segling/[i:id]', 'SeglingController#edit', 'segling-edit');
-        $this->router->map('POST', '/segling/[i:id]', 'SeglingController#save', 'segling-save');
-        $this->router->map('GET', '/segling/new', 'SeglingController#showCreate', 'segling-show-create');
-        $this->router->map('POST', '/segling/new', 'SeglingController#create', 'segling-create');
-        $this->router->map('POST', '/segling/delete/[i:id]', 'SeglingController#delete', 'segling-delete');
-        $this->router->map('POST', '/segling/medlem', 'SeglingController#saveMedlem', 'segling-medlem-save');
-        $this->router->map('POST', '/segling/medlem/delete', 'SeglingController#deleteMedlemFromSegling', 'segling-medlem-delete');
-
-        $this->router->map('GET', '/roller', 'RollController#list', 'roll-list');
-        $this->router->map('GET', '/roller/[i:id]/medlem', 'RollController#membersInRole', 'roll-medlemmar');
-
-        $this->router->map('GET', '/login', 'AuthController#showLogin', 'show-login');
-        $this->router->map('POST', '/login', 'AuthController#login', 'login');
-        $this->router->map('GET', '/logout', 'AuthController#logout', 'logout');
-        $this->router->map('POST', '/register', 'AuthController#register', 'register');
-        $this->router->map('GET', '/register/[a:token]', 'AuthController#activate', 'register-activate');
-        $this->router->map('GET', '/auth/bytlosenord', 'AuthController#showRequestPwd', 'show-request-password');
-        $this->router->map('POST', '/auth/bytlosenord', 'AuthController#sendPwdRequestToken', 'handle-request-password');
-        $this->router->map('GET', '/auth/bytlosenord/[a:token]', 'AuthController#showResetPassword', 'show-reset-password');
-        $this->router->map('POST', '/auth/sparalosenord', 'AuthController#resetAndSavePassword', 'reset-password');
-
-        $this->router->map('GET', '/user', 'UserController#home', 'user-home');
-
-        //Route all other urls to 404
-        $this->router->map('GET|POST', '*', 'HomeController#pageNotFound', '404');
+        // Routes are created from the Config/RouteConfig class
+        RouteConfig::createAppRoutes($this->router);
     }
 
-    private function dispatch($match, $request, $router)
+    private function dispatch($match, $request, $router): void
     {
         //If we have a string with a # then it's a controller action pair
         if (is_string($match['target']) && strpos($match['target'], "#") !== false) {
@@ -108,13 +68,13 @@ class Application
         }
     }
 
-    private function loadEnvironment()
+    private function loadEnvironment(): void
     {
         $dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . '/sl-webapp');
         $dotenv->load();
     }
 
-    private function loadConfig()
+    private function loadConfig(): void
     {
         $this->config = array_map(function ($value) {
             return $value === 'true' ? true : ($value === 'false' ? false : $value);
@@ -122,27 +82,27 @@ class Application
     }
 
 
-    public function getAppDir()
+    public function getAppDir(): string
     {
         return $_SERVER['DOCUMENT_ROOT'] . $this->config['APP_DIR'];
     }
 
-    public function getBaseUrl()
+    public function getBaseUrl(): string
     {
         return $this->config['APP_DIR'];
     }
 
-    public function getConfig(string $key)
+    public function getConfig(string $key): string
     {
         return $this->config[$key] ?? null;
     }
 
-    public function getRouter()
+    public function getRouter(): AltoRouter
     {
         return $this->router;
     }
 
-    private function setupSession()
+    private function setupSession(): void
     {
         ini_set('session.cookie_httponly', 1);
         ini_set('session.use_only_cookies', 1);
@@ -150,19 +110,19 @@ class Application
         Session::start();
     }
 
-    public function addMiddleware(MiddlewareInterface $middleware)
+    public function addMiddleware(MiddlewareInterface $middleware): void
     {
         $this->middlewares[] = $middleware;
     }
 
-    public function runMiddleware()
+    public function runMiddleware(): void
     {
         foreach ($this->middlewares as $middleware) {
             $middleware->handle();
         }
     }
 
-    public function run()
+    public function run(): void
     {
         // Match the current request
         $match = $this->router->match();
--- App/Config/RouteConfig.php
+++ App/Config/RouteConfig.php
@@ -2,8 +2,11 @@
 
 namespace App\Config;
 
+use AltoRouter;
+
 class RouteConfig
 {
+    //Used by middleware to know what pages does not require login
     public static $noLoginRequiredRoutes = [
         'show-login',
         'login',
@@ -16,4 +19,51 @@ class RouteConfig
         '404',
         'home'
     ];
+
+    // Central place to put all the applications routes
+    public static function createAppRoutes(AltoRouter $router)
+    {
+        $router->map('GET', '/', 'HomeController#index', 'home');
+
+        $router->map('GET', '/medlem', 'MedlemController#list', 'medlem-list');
+        $router->map('GET', '/medlem/json', 'MedlemController#listJson', 'medlem-list-json');
+        $router->map('GET', '/medlem/[i:id]', 'MedlemController#edit', 'medlem-edit');
+        $router->map('POST', '/medlem/[i:id]', 'MedlemController#save', 'medlem-save');
+        $router->map('GET', '/medlem/new', 'MedlemController#new', 'medlem-new');
+        $router->map('POST', '/medlem/new', 'MedlemController#insertNew', 'medlem-create');
+        $router->map('POST', '/medlem/delete', 'MedlemController#delete', 'medlem-delete');
+
+        $router->map('GET', '/betalning', 'BetalningController#list', 'betalning-list');
+        $router->map('GET', '/betalning/[i:id]', 'BetalningController#getBetalning', 'betalning-edit');
+        $router->map('GET', '/betalning/medlem/[i:id]', 'BetalningController#getMedlemBetalning', 'betalning-medlem');
+        $router->map('POST', '/betalning/create', 'BetalningController#createBetalning', 'betalning-create');
+        $router->map('POST', '/betalning/delete/[i:id]', 'BetalningController#deleteBetalning', 'betalning-delete');
+
+        $router->map('GET', '/segling', 'SeglingController#list', 'segling-list');
+        $router->map('GET', '/segling/[i:id]', 'SeglingController#edit', 'segling-edit');
+        $router->map('POST', '/segling/[i:id]', 'SeglingController#save', 'segling-save');
+        $router->map('GET', '/segling/new', 'SeglingController#showCreate', 'segling-show-create');
+        $router->map('POST', '/segling/new', 'SeglingController#create', 'segling-create');
+        $router->map('POST', '/segling/delete/[i:id]', 'SeglingController#delete', 'segling-delete');
+        $router->map('POST', '/segling/medlem', 'SeglingController#saveMedlem', 'segling-medlem-save');
+        $router->map('POST', '/segling/medlem/delete', 'SeglingController#deleteMedlemFromSegling', 'segling-medlem-delete');
+
+        $router->map('GET', '/roller', 'RollController#list', 'roll-list');
+        $router->map('GET', '/roller/[i:id]/medlem', 'RollController#membersInRole', 'roll-medlemmar');
+
+        $router->map('GET', '/login', 'AuthController#showLogin', 'show-login');
+        $router->map('POST', '/login', 'AuthController#login', 'login');
+        $router->map('GET', '/logout', 'AuthController#logout', 'logout');
+        $router->map('POST', '/register', 'AuthController#register', 'register');
+        $router->map('GET', '/register/[a:token]', 'AuthController#activate', 'register-activate');
+        $router->map('GET', '/auth/bytlosenord', 'AuthController#showRequestPwd', 'show-request-password');
+        $router->map('POST', '/auth/bytlosenord', 'AuthController#sendPwdRequestToken', 'handle-request-password');
+        $router->map('GET', '/auth/bytlosenord/[a:token]', 'AuthController#showResetPassword', 'show-reset-password');
+        $router->map('POST', '/auth/sparalosenord', 'AuthController#resetAndSavePassword', 'reset-password');
+
+        $router->map('GET', '/user', 'UserController#home', 'user-home');
+
+        //Route all other urls to 404
+        $router->map('GET|POST', '*', 'HomeController#pageNotFound', '404');
+    }
 }
