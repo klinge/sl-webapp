02c902b92de4112ed6395c948f6cb4506a63e446
Johan Klinge <johan.klinge@gmail.com>
2024-10-02 19:21:17 +0200

    Fixed bug in CsrfMiddleware

--- App/Middleware/CsrfMiddleware.php
+++ App/Middleware/CsrfMiddleware.php
@@ -8,17 +8,27 @@ use App\Utils\Session;
 
 class CsrfMiddleware extends BaseMiddleware implements MiddlewareInterface
 {
+    private $excludedPaths = ['/webhooks', '/home'];
+
     public function handle(): void
     {
         if (!isset($_SESSION['csrf_token'])) {
             $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
         }
-        //echo "In CsrfMiddleware. Session token is: " . $_SESSION['csrf_token'];
+
+        // Check if the current path is in the excluded list
+        $currentPath = parse_url($this->request['REQUEST_URI'], PHP_URL_PATH);
+        foreach ($this->excludedPaths as $excludedPath) {
+            if (strpos($currentPath, $excludedPath) === 0) {
+                $this->app->getLogger()->debug('Call to a path that excludes csrf protection: ' . $currentPath);
+                return;
+            }
+        }
 
         if ($this->request['REQUEST_METHOD'] === 'POST') {
             $token = $_POST['csrf_token'] ?? '';
             if (!hash_equals($_SESSION['csrf_token'], $token)) {
-                $this->app->getLogger()->warning('CSRF token mismatch');
+                $this->app->getLogger()->warning('CSRF token mismatch. Uri was: ' . $currentPath . "Called by; " . $this->request['REMOTE_ADDR']);
                 //Set different responses depending on if it was an ajax request or not
                 if ($this->isAjaxRequest()) {
                     $this->sendJsonResponse(['status' => 'error', 'message' => 'Error validating csrf token'], 401);
