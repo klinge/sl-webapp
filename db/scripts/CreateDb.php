103ad19ecadf0dba51cfc8f213ce16f17c69fb8d
Johan Klinge <johan.klinge@gmail.com>
2024-10-08 10:18:52 +0200

    Setting php version 8.2 in phpstan config

--- db/scripts/CreateDb.php
+++ db/scripts/CreateDb.php
@@ -5,27 +5,60 @@ declare(strict_types=1);
 namespace App\Db\Scripts;
 
 use App\Application;
+use Monolog\Logger;
 
 /**
- * MedlemController handles operations related to members (medlemmar).
- *
- * This controller manages CRUD operations for members, including listing,
- * editing, creating, and deleting member records. It also handles related
- * operations such as managing roles and payments for members.
+ * CreateDb handles database creation and management operations.
  */
 class CreateDb
 {
-    /**
-     * @var Application The application instance
-     */
     private Application $app;
-    private $db;
-    private $logger;
+    private string $db;
+    private Logger $logger;
 
-    private function __construct()
+    /**
+     * Private constructor to prevent direct instantiation.
+     * This class should be used statically.
+     */
+    protected function __construct()
     {
         $this->app = new Application();
         $this->db = $this->app->getRootDir() . '/db/sl-prod.sqlite';
         $this->logger = $this->app->getLogger();
+    }
+
+    private function recreateDb()
+    {
+        $this->backup();
+        $this->createDb();
+        $this->moveDb();
+    }
+
+    private function backup()
+    {
+        $this->logger->debug('Backing up database');
+        $backup = $this->app->getRootDir() . '/db/sl-prod.sqlite.bak';
+        if (file_exists($backup)) {
+            unlink($backup);
+        }
+        copy($this->db, $backup);
+        if (file_exists($this->db)) {
+            unlink($this->db);
+        }
+        $this->logger->info('Backup of database is now in: ' . $backup);
+    }
+
+    private function createDb()
+    {
+        //TODO
+    }
+
+    private function moveDb()
+    {
+        $this->logger->info('Moving new database in place');
+        $newDb = $this->app->getRootDir() . '/db/sl-prod.sqlite';
+        if (file_exists($newDb)) {
+            //TODO
+        }
     }
 }
