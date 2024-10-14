d79e8eb05c158cb4d7084a66677d3b9f80c5359a
Johan Klinge <johan.klinge@gmail.com>
2024-10-14 17:15:34 +0200

    Removed unused view

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,29 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //Code to toggle dark mode
-    const darkModeIcon = document.getElementById('darkModeIcon');
-    if (darkModeIcon) {
-      darkModeIcon.addEventListener('click', function () {
-        this.classList.toggle('bi-moon');
-        this.classList.toggle('bi-sun');
-        let theme = document.documentElement.getAttribute('data-bs-theme');
-        let nav = document.getElementById('slnav');
-        if (theme === 'dark') {
-          document.documentElement.removeAttribute('data-bs-theme');
-          nav.style.color = "white";
-        } else {
-          document.documentElement.setAttribute('data-bs-theme', 'dark');
-          nav.style.removeProperty('color');
-        }
-      });
-    }
-  });
-  
 
-   // Add this new function to check and update Turnstile theme
-   function updateTurnstileTheme() {
+// Add this new function to check and update Turnstile theme
+function updateTurnstileTheme() {
     const bodyTheme = document.documentElement.getAttribute('data-bs-theme');
     const turnstileWidgets = document.querySelectorAll('.cf-turnstile');
-
+    
     if (bodyTheme === 'dark') {
         turnstileWidgets.forEach(widget => {
             widget.setAttribute('data-theme', 'dark');
@@ -46,21 +26,21 @@ function onLoginSubmit() {
     //And then submit the form
     loginForm.submit();
     return true;
-};
+}
 
 function onRegisterSubmit() {
     const form = document.getElementById('registerForm');
     //And then submit the form
     form.submit();
     return true;
-};
+}
 
 function onFormSubmit() {
     const form = document.getElementById('bytlosenForm');
     //And then submit the form
     form.submit();
     return true;
-};
+}
 
 function checkPasswords() {
     const password = document.getElementById('password').value;
